9fd6794912c2f7439f827236da5dfef4b365fcb1
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-01-06 08:00:31 +0100

    export scss variables

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../styles/Sidebar.css"
 import { BsFacebook } from 'react-icons/bs';
+import themeVariables from '../styles/theme-variables.module.scss'
 
 function Sidebar() {
+  console.log(themeVariables);
   return (
     <div className="sidebar">
         <div className="sidebar__header">
             <img className="sidebar__ava" alt="ava" src={require("../assets/photo.png")}/>
             <h1>Bc. Dmytro Kagirov</h1>
             <h2>Student, Web Developer</h2>
-            <BsFacebook color='white'/>
+            <BsFacebook color={themeVariables["light-grey"]}/>
         </div>
     </div>
   )
