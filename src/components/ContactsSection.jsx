b8395d7864ace3e80e553e7a70dcf6328e029427
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-01-14 16:10:29 +0100

    Animation for links added

--- src/components/ContactsSection.jsx
+++ src/components/ContactsSection.jsx
@@ -15,11 +15,19 @@ function ContactsSection() {
                 <h3>Devpost:</h3>
             </div>
             <div className='contacts__data'>
-                <a href='mailto:dmytro27kagirov@gmail.com'><span>dmytro27kagirov@gmail.com</span></a>
-                <a href='https://github.com/Dmytro27Ind'><span>github.com/Dmytro27Ind</span></a>
-                <a><span>@lyn10i</span></a>
+                <a href='mailto:dmytro27kagirov@gmail.com' target="_blank" rel="noreferrer">
+                    <span>dmytro27kagirov@gmail.com</span>
+                </a>
+                <a href='https://github.com/Dmytro27Ind' target="_blank" rel="noreferrer">
+                    <span>github.com/Dmytro27Ind</span>
+                </a>
+                <a href='https://t.me/lyn10i' target="_blank" rel="noreferrer">
+                    <span>@lyn10i</span>
+                </a>
                 <span>+421 951 338 470</span>
-                <a><span>devpost.com/dmytro27kagirov</span></a>
+                <a href='https://devpost.com/dmytro27kagirov' target="_blank" rel="noreferrer">
+                    <span>devpost.com/dmytro27kagirov</span>
+                </a>
             </div>
         </div>
     </div>
